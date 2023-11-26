9eead97a8e2c9b4ae79f7ebd43204efdc04a8d79
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-11-26 14:08:10 -0500

    Time: 46 ms, Memory: 16.4 MB - LeetHub

--- house-robber/house-robber.py
+++ house-robber/house-robber.py
@@ -1,8 +1,7 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        @cache
-        def dp(house_index):
+        def dp(house_index, memo = {}):
             # If this is the only house (first), rob it
             if house_index == 0:
                 return nums[house_index]
@@ -11,10 +10,15 @@ class Solution:
             if house_index == 1:
                 return max(nums[0], nums[1])
             
+            # If we have computed result previously (memoized), use it
+            if house_index in memo:
+                return memo[house_index]
+            
             # At any house, we have two options:
             # Rob it => money at second last house + curr house
             # Dont rob it => money we had at prev house
-            return max(dp(house_index - 1), dp(house_index - 2) + nums[house_index])
+            memo[house_index] = max(dp(house_index - 1), dp(house_index - 2) + nums[house_index])
+            return memo[house_index]
             
         return dp(len(nums) - 1)
     
