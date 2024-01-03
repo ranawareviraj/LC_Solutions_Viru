c2196895edaf0b3b77388eda742d97e8a1751d85
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2024-01-03 13:23:24 -0500

    Time: 34 ms, Memory: 17.2 MB - LeetHub

--- check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
+++ check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
@@ -1,3 +1,16 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        return len(set(sentence)) == 26
+        # Build Frequency map
+        char_freq = [0] * 26
+        ord_a = ord('a')
+        for ch in sentence:
+            char_freq[ord(ch) - ord_a] += 1
+        
+        # If any character has freq == 0, its not Pangram
+        for freq in char_freq:
+            if freq == 0:
+                return False
+        
+        # If we reach here, then we have all alphabets in the sentence - Pangram
+        return True
+            
