0563baf574f6e2f59aa4e2a5e458b4999d778d2e
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2024-01-23 20:09:41 -0500

    Time: 1 ms, Memory: 41.6 MB - LeetHub

---
+++ check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        boolean[] seen = new boolean[26];
+        for(char ch : sentence.toCharArray()){
+            seen[ch - 'a'] = true;
+        }
+        
+        for(int i = 0; i < 26; i++){
+            if(seen[i] == false){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
